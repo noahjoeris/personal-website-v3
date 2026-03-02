9b4bb479c2755200348aa22f45fb8665c7214121
Noah Joeris <noahjoeris@gmail.com>
2026-03-02 17:02:57 +0200

    minor design change

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -6,12 +6,12 @@ import { WatermarkText } from '@/components/watermark-text'
 
 export function HeroSection() {
   return (
-    <section className="relative overflow-hidden bg-black lg:min-h-screen">
-      <div className="absolute inset-0 hidden bg-gradient-to-br from-[hsl(182,30%,14%)] via-[hsl(182,30%,12%)] to-[hsl(185,25%,10%)] lg:block" />
+    <section className="relative overflow-hidden bg-background lg:min-h-screen">
+      <div className="absolute inset-0 hidden bg-linear-to-br from-[hsl(182,30%,14%)] via-[hsl(182,30%,12%)] to-[hsl(185,25%,10%)] lg:block" />
 
-      <div className="absolute inset-x-0 top-0 h-[460px] bg-gradient-to-br from-[hsl(182,30%,14%)] via-[hsl(182,30%,12%)] to-[hsl(185,25%,10%)] md:h-[620px] lg:hidden" />
+      <div className="absolute inset-x-0 top-0 h-[460px] bg-linear-to-br from-[hsl(182,30%,14%)] via-[hsl(182,30%,12%)] to-[hsl(185,25%,10%)] md:h-[620px] lg:hidden" />
 
-      <div className="relative h-[460px] w-full md:h-[620px] lg:absolute lg:inset-0 lg:z-10 lg:h-full">
+      <div className="relative mt-3 h-[460px] w-full md:mt-0 md:h-[620px] lg:absolute lg:inset-0 lg:z-10 lg:h-full">
         <div className="relative z-20 h-full w-full">
           <HeroImage />
         </div>
@@ -20,7 +20,7 @@ export function HeroSection() {
           <WatermarkText />
         </div>
 
-        <div className="pointer-events-none absolute inset-x-0 bottom-0 z-[15] h-40 bg-gradient-to-b from-transparent via-black/6 to-black/18 md:h-48 lg:h-[24vh] lg:via-black/8 lg:to-black/16" />
+        <div className="pointer-events-none absolute inset-x-0 bottom-0 z-15 h-40 bg-linear-to-b from-transparent via-black/6 to-black/18 md:h-48 lg:h-[24vh] lg:via-black/8 lg:to-black/16" />
       </div>
 
       <div className="relative z-30 px-6 pb-10 pt-8 md:px-10 md:pb-14 lg:flex lg:min-h-screen lg:flex-col lg:justify-center lg:px-16 lg:pb-20 lg:pt-24">
